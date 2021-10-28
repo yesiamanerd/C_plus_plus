30bdaff2a28c4399d66bdad040c9a8cfb3baba45
yesiamanerd <yesiamanerd@gmail.com>
2021-10-27 20:51:20 -0400

    final input output operator

---
+++ Early_classes/Input - Output Operators/Shapes/Shapes.cpp
@@ -0,0 +1,29 @@
+// (Displaying Shapes with Asterisks) 
+// Write a program that prints a box, an oval, an arrow and
+// a diamond as follows:
+// *********     ***       *        *
+// *       *   *     *    ***      * *
+// *       *  *       *  *****    *   *
+// *       *  *       *    *     *     *
+// *       *  *       *    *    *       *
+// *       *  *       *    *     *     *
+// *       *  *       *    *      *   *
+// *       *   *     *     *       * *
+// *********     ***       *        *
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout << "*********     ***       *        *" << endl;
+    cout << "*       *   *     *    ***      * *" << endl;
+    cout << "*       *  *       *  *****    *   *" << endl;
+    cout << "*       *  *       *    *     *     *" << endl;
+    cout << "*       *  *       *    *    *       *" << endl;
+    cout << "*       *  *       *    *     *     *" << endl;
+    cout << "*       *  *       *    *      *   *" << endl;
+    cout << "*       *   *     *     *       * *" << endl;
+    cout << "*********     ***       *        *" << endl;
+
+    return 0;
+}
