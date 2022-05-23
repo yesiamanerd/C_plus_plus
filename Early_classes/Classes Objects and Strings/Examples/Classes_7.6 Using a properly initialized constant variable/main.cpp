500393ab4fd9c32469c4280b6571726d04791bca
gbrophy <yesiamanerd@gmail.com>
2022-05-23 14:00:08 -0400

    code changes for practice

---
+++ Early_classes/Classes Objects and Strings/Examples/Classes_7.6 Using a properly initialized constant variable/main.cpp
@@ -0,0 +1,11 @@
+// Using a properly initialized constant variable
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    const int x = 7;
+
+
+    cout <<  "\nThe value of constant variable is: " << x << "\n" << endl;
+}
